0f553504bb742c9950064fd23db5f4359d8317be
bibis <bertha@gmail.com>
2025-03-28 16:38:56 +0100

    clase 4 fase 1

--- Angular-IV/VeladaAngular/src/app/app.routes.ts
+++ Angular-IV/VeladaAngular/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { JugadorComponent } from './componentes/jugador/jugador.component';
+import { JugadoresComponent } from './componentes/jugadores/jugadores.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { ContactComponent } from './componentes/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'navbar', component: NavbarComponent},
+  {path: 'footer', component: FooterComponent},
+  {path: 'jugador', component: JugadorComponent},
+  {path: 'jugadores', component: JugadoresComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
